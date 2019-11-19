4b72dcf1f059a1e9f7310fd463a6afe160e524f7
BaekRui <40696821+BaekElk19@users.noreply.github.com>
2019-11-19 14:10:19 +0800

    Add files via upload

---
+++ Mooc/__init__.py
@@ -0,0 +1,13 @@
+'''
+Mooc下载器版本  3.4.1
+作者            PyJun
+邮箱            py.jun@qq.com
+github          https://github.com/PyJun/Mooc_Downloader
+博客            https://blog.csdn.net/qq_16166591/article/details/85249743
+'''
+
+__version__ = 'Mooc-3.4.1（不限速）'
+__author__  = 'BaekElk19'
+__email__   = '949378798@qq.com'
+__github__  = 'https://github.com/BaekElk19'
+__blog__    = 'https://blog.csdn.net/qq_16166591/article/details/85249743'
